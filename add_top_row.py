8fd433880e62222676898b5e37d923c32ea1cc08
Frank Hu <humour114@gmail.com>
2015-11-16 00:01:09 +0800

    first commit: average

--- add_top_row.py
+++ add_top_row.py
@@ -12,12 +12,19 @@ output.write('Use_city,Line_name,Terminal_id,Card_id,Create_city,Deal_time,Card_
 def to_datetime(time):
     return '{}-{}-{} {}:00:00'.format(time[:4],time[4:6],time[6:8],time[8:])
 
-for i in range(100):
+'''
+for i in range(100): # test for range
     s = f.readline()
     time = s[-15:-5] # magic numbers: slice for datetime
     #print(to_datetime(time))
     output.write(s[:-15]+to_datetime(time)+s[-5:])
+'''
 
+# extract whole file
 
+for line in f:
+    time = line[-15:-5] # magic numbers: slice for datetime
+    #print(to_datetime(time))
+    output.write(line[:-15]+to_datetime(time)+line[-5:])  
 
 #print(to_datetime(str(2014111206)))
