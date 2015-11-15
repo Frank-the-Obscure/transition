599a54dc0c1a543b912a582b659da455f87b9c70
Frank Hu <humour114@gmail.com>
2015-11-15 22:24:57 +0800

    first data process

---
+++ add_top_row.py
@@ -0,0 +1,23 @@
+# add top row for csv format files
+# author: Frank Hu, Frank-the-Obscure @ GitHub
+
+#import re
+
+f = open('gd_train_data.txt', encoding='utf-8')
+
+output = open('gd_train_data_100.csv', 'w', encoding='utf-8')
+
+output.write('Use_city,Line_name,Terminal_id,Card_id,Create_city,Deal_time,Card_type\n')
+
+def to_datetime(time):
+    return '{}-{}-{} {}:00:00'.format(time[:4],time[4:6],time[6:8],time[8:])
+
+for i in range(100):
+    s = f.readline()
+    time = s[-15:-5] # magic numbers: slice for datetime
+    #print(to_datetime(time))
+    output.write(s[:-15]+to_datetime(time)+s[-5:])
+
+
+
+#print(to_datetime(str(2014111206)))
