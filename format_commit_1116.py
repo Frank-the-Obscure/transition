c1748d0478dfa59ccc824f9cd5200785ffcac4c1
Frank Hu <humour114@gmail.com>
2015-11-17 22:35:39 +0800

    1116: second commit

---
+++ format_commit_1116.py
@@ -0,0 +1,19 @@
+# author: Frank Hu, Frank-the-Obscure @ GitHub
+# 读取数据
+# add top row for csv format files
+
+import re
+
+f = open('commit_1116.txt', encoding='utf-8')
+
+output = open('gd_predict_1116.txt', 'w', encoding='utf-8')
+
+t = re.compile(',')
+
+for line in f:
+	columns = t.split(line)
+	#print(columns)
+	hour = int(columns[2])
+	if hour < 10:
+		columns[2] = '0' + columns[2]
+	output.write(','.join(columns))
